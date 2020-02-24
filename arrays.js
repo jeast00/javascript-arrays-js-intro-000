06bbb6182c41fce1670576840469338c4d1404ba
Jonathan East <jonathan_east@ymail.com>
2020-02-24 05:38:42 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,7 +29,7 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.pop(0, array.length - 1);
+  return array.pop();
 }
 
 function removeElementFromEndOfArray(array) {
