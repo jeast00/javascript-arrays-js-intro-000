c5d092dc83db4e7ac959e3adea7d2bb9dec927e4
Jonathan East <jonathan_east@ymail.com>
2020-02-24 03:59:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+addElementToBeginningOfArray(array, element) {
+
+}
+
+destructivelyAddElementToBeginningOfArray(array, element) {
+
+}
