adc806d15e3db06cb16977b346da062d78038f69
Jonathan East <jonathan_east@ymail.com>
2020-02-24 04:06:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,1 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-
-addElementToBeginningOfArray(array, element) {
-  return array.unshift(element);
-}
-
-destructivelyAddElementToBeginningOfArray(array, element) {
-
-}
